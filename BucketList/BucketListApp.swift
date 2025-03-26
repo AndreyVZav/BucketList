5d60eb92ef1ffd1019a32906fe04dcf16dd2d1c3
AndreyVZav <zav.av@bk.ru>
2025-03-26 10:15:04 +0300

    Initial Commit

---
+++ BucketList/BucketListApp.swift
@@ -0,0 +1,17 @@
+//
+//  BucketListApp.swift
+//  BucketList
+//
+//  Created by Андрей Завадский on 26.03.2025.
+//
+
+import SwiftUI
+
+@main
+struct BucketListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
