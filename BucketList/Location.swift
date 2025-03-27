2ff21aedfb83d3b1bc0ca3eacc81e19387c0cee9
AndreyVZav <zav.av@bk.ru>
2025-03-27 15:05:37 +0300

    Improving our map annotations

--- BucketList/Location.swift
+++ BucketList/Location.swift
@@ -5,6 +5,7 @@
 //  Created by Андрей Завадский on 27.03.2025.
 //
 import SwiftUI
+import MapKit
 
 struct Location: Codable, Equatable, Identifiable {
     let id: UUID
@@ -12,4 +13,15 @@ struct Location: Codable, Equatable, Identifiable {
     var description: String
     var latitude: Double
     var longitude: Double
+    
+    var coordinate: CLLocationCoordinate2D {
+        CLLocationCoordinate2D(latitude: latitude, longitude: longitude)
+    }
+    
+    static let example = Location(id: UUID(), name: "Buckingham Palace", description: "Lit by over 40,000 lightbulbs.", latitude: 51.501, longitude: -0.141)
+    
+    static func ==(lhs: Location, rhs: Location) -> Bool {
+        lhs.id == rhs.id
+    }
+    
 }
