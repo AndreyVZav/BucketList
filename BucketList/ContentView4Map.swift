deccda2a1b920f1c76020e457eb2009356dc845c
AndreyVZav <zav.av@bk.ru>
2025-03-27 11:51:33 +0300

    Integrating MapKit with SwiftUI

---
+++ BucketList/ContentView4Map.swift
@@ -0,0 +1,64 @@
+//
+//  ContentView4Map.swift
+//  BucketList
+//
+//  Created by Андрей Завадский on 27.03.2025.
+//
+import SwiftUI
+import MapKit
+
+struct Location: Identifiable {
+    let id = UUID()
+    var name: String
+    var coordinate: CLLocationCoordinate2D
+}
+
+struct ContentView4: View {
+    @State private var position = MapCameraPosition.region(
+        MKCoordinateRegion(
+            center: CLLocationCoordinate2D(latitude: 51.507222, longitude: -0.1275),
+            span: MKCoordinateSpan(latitudeDelta: 1, longitudeDelta: 1)
+        )
+    )
+    let locations = [
+        Location(name: "Buckingham Palace", coordinate: CLLocationCoordinate2D(latitude: 51.501, longitude: -0.141)),
+        Location(name: "Tower of London", coordinate: CLLocationCoordinate2D(latitude: 51.508, longitude: -0.076))
+    ]
+    var body: some View {
+        VStack {
+            MapReader { proxy in
+                Map()
+                    .onTapGesture { position in
+                        if let coordinate = proxy.convert(position, from: .local) {
+                            print(coordinate)
+                        }
+                    }
+            }
+            
+            HStack(spacing: 50) {
+                Button("Paris") {
+                    position = MapCameraPosition.region(
+                        MKCoordinateRegion(
+                            center: CLLocationCoordinate2D(latitude: 48.8566, longitude: 2.3522),
+                            span: MKCoordinateSpan(latitudeDelta: 1, longitudeDelta: 1)
+                        )
+                    )
+                }
+                
+                Button("Tokyo") {
+                    position = MapCameraPosition.region(
+                        MKCoordinateRegion(
+                            center: CLLocationCoordinate2D(latitude: 35.6897, longitude: 139.6922),
+                            span: MKCoordinateSpan(latitudeDelta: 1, longitudeDelta: 1)
+                        )
+                    )
+                }
+            }
+        }
+
+    }
+}
+
+#Preview {
+    ContentView4()
+}
